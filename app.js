685f414747e238bed8d59c2af5afcac4b8b2e142
MryImani <77524419+MryImani@users.noreply.github.com>
2021-04-20 12:42:55 +0430

    insert some modules

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const bodyParser = require('body-parser');
-const express=require('express');
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/",function(req,res){
-    var today=new Date();
-    if(today.getDay()=== 5)
-    {
-        res.send("Yeh, it's the weekend!")
+const express = require('express');
+const app = express();
+let items = [" بیدار شدن از خواب",
+    "ورزش",
+    "صبحانه",
+    "مطالعه",
+    " استراحت"];
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
-    else{
-        res.send("Boo! I have to work!");
-    }
-    
+    let day = today.toLocaleDateString("fa-IR", options);
+    res.render('list', { kindOfDay: day, newListItems: items });
+})
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 })
 
 
 
-
-
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("server is running on port 3000");
 })
